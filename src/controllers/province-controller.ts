46ed441c90593b9533fc34142b98bea38df8d39a
Đào Xuân Duy <daoxuanduy@Mac-cua-Duy.local>
2025-04-16 23:19:56 +0700

    feature province district ward

--- src/controllers/province-controller.ts
+++ src/controllers/province-controller.ts
@@ -4,7 +4,7 @@ export class ProvinceController {
     async index(req: any, res: any) {
         const { page, page_size, query } = req.query
         const province = new ProvinceModel();
-        const data = await province.getAll(page, page_size, query);
+        const data = await province.getAll(Number(page || 1), Number(page_size || 10), query);
         res.status(200).json(data);
     }
 }
