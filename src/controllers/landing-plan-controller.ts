6e1709a89eba7297c1c1435a8d792852d6ed959e
Thaiht <thaiht@ows.com.vn>
2025-05-29 02:16:12 +0700

    feat: api list ban do quy hoach

--- src/controllers/landing-plan-controller.ts
+++ src/controllers/landing-plan-controller.ts
@@ -2,20 +2,25 @@ import { LandingPlanModel } from "../models";
 
 export class LandingPlanController {
     async findMapByLatLon(req: any, res: any) {
-        const { lat, lon } = req.query;
+        const { lat, lon, radius } = req.query;
         const latFloat = parseFloat(lat as string);
         const lonFloat = parseFloat(lon as string);
-
+        const radiusFloat = radius !== undefined ? parseFloat(radius as string) : 0;
         if (isNaN(latFloat) || isNaN(lonFloat)) {
             return res.status(400).json({ message: "Invalid latitude or longitude" });
         }
 
         try {
             const landingPlanModel = new LandingPlanModel();
-            const result = await landingPlanModel.findMapByLatLon(latFloat, lonFloat);
-            res.status(200).json(result);
+            if (radiusFloat > 0) {
+                const result = await landingPlanModel.listMapInRange(latFloat, lonFloat, radiusFloat);
+                res.status(200).json(result);
+            } else {
+                const result = await landingPlanModel.findMapByLatLon(latFloat, lonFloat);
+                res.status(200).json(result);
+            }
         } catch (error) {
-            res.status(500).json({ message: "Error fetching map by coordinates", error });
+            res.status(500).json({ message: "Error fetching map(s)", error });
         }
     }
 
